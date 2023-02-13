1664033be9cb07248ee71a295184a057faf5cfbd
Henrique91 <henriquesilvacorrea91@gmail.com>
2023-02-13 17:24:10 -0300

    UC-12 Teste Back End

---
+++ CalculadoraIMC/TesteIMC/ClassificarIMCTeste.cs
@@ -0,0 +1,23 @@
+﻿using CalculadoraIMC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteIMC
+{
+    [TestClass]
+    public class ClassificarIMCTeste
+    {
+        [TestMethod]
+        public void ClassificarIMC() 
+        {
+            double imc = 28;
+
+            var classificacaoIMC = Operacoes.ClassificarIMC(imc);
+
+            Assert.AreEqual("Sobrepeso",classificacaoIMC);
+        }
+    }
+}
